f63c1b5714f738663703ad98ebcc6c82f7042131
Sravulap <91766439+Sravulap@users.noreply.github.com>
2023-05-02 13:49:11 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function memory(){
+   document.getElementById("Memory").src = "MemoryBcell.png";
+}
+function plasma(){
+ document.getElementById("Memory").src = "PlasmaBcell.png";
+}
+
+function neutrophils(){
+    document.getElementById("Memory").src = "Neutrophils.png";
+}
+
+function eosinophils(){
+    document.getElementById("Memory").src = "Eosinophils.png";
+}
+
+function basophils(){
+    document.getElementById("Memory").src = "basophils.png";
+}
+
+function helper(){
+    document.getElementById("Memory").src = "HelperTcell.png";
+}
+
+
+
